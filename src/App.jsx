3667e8dfa0e85652d062a62f8608629ff6346f3a
Doomsx <127058006+doomsx@users.noreply.github.com>
2024-09-13 19:51:53 +0800

    fetch and store data from the apod api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-
+import axios from "axios"
+import { useState, useEffect } from "react"
 const API_KEY = import.meta.env.VITE_API_KEY
 
+
 function App() {
+
+    const [result, setResult] = useState([])
+
+    const img = async () => {
+        try {
+            const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
+            const response = await axios.get(url)
+            setResult({
+                date: response.data.date,
+                imgUrl: response.data.url,
+                title: response.data.title,
+                explanation: response.data.explanation
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        img()
+    }, [])
+
     return (
         <>
-            <h1 className="text-3xl font-black"> Hello World </h1>
+            <h1>{result.title}</h1>
+            <p>{result.date}</p>
+            <img src={result.imgUrl} alt="" />
         </>
     )
 }
